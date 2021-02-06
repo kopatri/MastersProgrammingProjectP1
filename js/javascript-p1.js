6fcb96656970842ce74962fabd033e960fbf912b
eformuPK <shiftink.de@gmail.com>
2021-02-06 20:45:33 +0000

    advanced breed selection and display not fully tested, but looks good

--- js/javascript-p1.js
+++ js/javascript-p1.js
@@ -3,6 +3,8 @@ var dog_arr = new Array(); //Uncaught SyntaxError: redeclaration of let
 var dog_breeds_arr = new Array();
 var splitted_breeds_arr = new Array(); // sorted array of breeds with 10 dog breed names per index
 var cur_open_cards_arr = new Array();
+var cur_breed_selection_id_arr = new Array(); //current selection of checked breed id checkboxes
+var cur_breed_selection_name_arr = new Array(); //current selection of checked breed names checkboxes
 
 var cur_breeds_index = 0; //Default index value for splitted breeds arr
 var playset_pairs = 0;
@@ -12,58 +14,68 @@ var correct_selected_pairs = 0;
 //user input related data and functions
 var highscore = { easy: "No game played!", normal: "No game played!", hard: "No game played!" };
 
-//Get random dogs
-async function getDogs(req_dog_amount) {
-    //requested different dogs
-    console.log("Requested dog amount: " + req_dog_amount);
-    for (let i = 0; i < req_dog_amount; i++) {
-        try {
-            let response = await fetch("https://api.thedogapi.com/v1/images/search")
-            let dog = await response.json()
-            dog_arr.push(dog);
-        }
-        catch (error) {
-            console.log("error", error);
-        }
-    }
-    printDogAll();
-    duplicateDogCards()
-    shuffleDogCards();
-    printDogCards();
-}
-
+//Print the breeds to corresponding HTML checkboxes
 getBreeds()
 async function getBreeds() {
     try {
         let response = await fetch("https://api.thedogapi.com/v1/breeds")
         let breeds = await response.json()
+        //console.log(breeds);
         splitBreeds(breeds);
     }
     catch (error) {
         console.log("error", error);
     }
-
     printBreeds();
 }
 
+//Get dogs based on their breed id and their limit or get completely random dogs by limit
+async function getDogs(requested_dog_pairs, requested_breed_id) {
+
+    console.log("Requested dog amount: " + requested_dog_pairs);
+    console.log("Requested breed id: " + requested_breed_id);
+
+
+    let query_string = "";
+    if (requested_breed_id != undefined) {
+        query_string = `https://api.thedogapi.com/v1/images/search?limit=${requested_dog_pairs}&mime_types=jpg&breed_id=${requested_breed_id}`;
+    }
+    else {
+        query_string = `https://api.thedogapi.com/v1/images/search?limit=${requested_dog_pairs}&mime_types=jpg`;
+    }
+
+    let dogs = new Array();
+    try {
+        let response = await fetch(query_string);
+        dogs = await response.json()
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+    //Push each single dog in an 2D dog array
+    for (let dog of dogs) {
+        dog_arr.push(dog)
+    }
+
+    if (playset_pairs == dog_arr.length) {
+        duplicateDogCards();
+        shuffleDogCards();
+        printDogCardsToHTML();
+    }
+}
 
 function splitBreeds(breeds) {
     //Get all breed names in one arry independently from the amount of different breeds
     for (let i = 0; i < breeds.length; i++) {
-        dog_breeds_arr.push(breeds[i].name);
+        dog_breeds_arr.push({ name: breeds[i].name, id: breeds[i].id });
     }
-    console.log(dog_breeds_arr);
+    //console.log(dog_breeds_arr);
 
-    //Create an new array() for at least 10 breeds(breed_pack_size) at each index and fill it 
+    //Create an new array() for at least 10 breed names(breed_pack_size) at each index and fill it 
     let breed_pack_size = 10;
     for (var i = 0; i < dog_breeds_arr.length; i = i + breed_pack_size) {
         splitted_breeds_arr.push(dog_breeds_arr.slice(i, i + breed_pack_size));
     }
-    console.log(splitted_breeds_arr);
-    let dogfourtytwo = splitted_breeds_arr[3][1];
-    console.log(dogfourtytwo);
-
-
 }
 
 function previousBreed() {
@@ -80,23 +92,54 @@ function nextBreed() {
     shiftBreedsList(cur_breeds_index);
 }
 
-function shiftBreedsList(breeds_index){
+function shiftBreedsList(breeds_index) {
     //If no breeds_index is specified, set default to current breeds index to 0
     if (breeds_index == undefined) {
-        console.log("Breeds index set 0");
+        //console.log("Breeds index set 0");
         cur_breeds_index = 0;
     } else if (breeds_index == -1) {
-        console.log("Breeds index set end of list");
+        //console.log("Breeds index set end of list");
         cur_breeds_index = splitted_breeds_arr.length - 1;
     }
     else if (breeds_index > splitted_breeds_arr.length - 1) {
-        console.log("Breeds index set beginning of list");
+        //console.log("Breeds index set beginning of list");
         cur_breeds_index = 0;
     } else {
-        console.log("Breeds index set value between beginning and end");
+        //console.log("Breeds index set value between beginning and end");
         cur_breeds_index = breeds_index;
     }
     document.getElementById(`breed-list-${cur_breeds_index}`).classList.add('active');
+}
+
+function breedChanged(el) {
+    if (el.checked) {
+        console.log("Checked: " + el.id);
+        cur_breed_selection_id_arr.push(el.id);
+        cur_breed_selection_name_arr.push(el.value);
+    } else {
+        console.log("Not checked: " + el.id);
+
+        //Array for breed ids
+        let to_uncheck_id = cur_breed_selection_id_arr.indexOf(el.id);
+        if (to_uncheck_id > -1) {
+            cur_breed_selection_id_arr.splice(to_uncheck_id, 1);
+        }
+
+        //Array for breed names to display
+        let to_uncheck_name = cur_breed_selection_name_arr.indexOf(el.value);
+        if (to_uncheck_name > -1) {
+            cur_breed_selection_name_arr.splice(to_uncheck_name, 1);
+        }
+    }
+
+    let selected_breeds = "Selected breeds:";
+    for (let breed_name of cur_breed_selection_name_arr) {
+        selected_breeds = selected_breeds + " " + breed_name + ",";
+    }
+    //remove last "," from string
+    selected_breeds = selected_breeds.slice(0, -1);
+
+    document.getElementById("selected_breeds").innerHTML = selected_breeds;
 }
 
 function printBreeds() {
@@ -106,7 +149,6 @@ function printBreeds() {
 
     let breedsHTML = "";
 
-    //<div class="breeds" id="breeds-list-"></div>
     for (let i = 0; i < splitted_breeds_arr.length; i++) {
         let dog_breeds_to_print_arr = new Array();
         dog_breeds_to_print_arr = splitted_breeds_arr[i];
@@ -114,28 +156,17 @@ function printBreeds() {
         breedsHTML = breedsHTML +
             `<div class="carousel-item" id="breed-list-${i}">
              <div class="breeds" >`;
-        for (let breed_name of dog_breeds_to_print_arr) {
+        for (let breed of dog_breeds_to_print_arr) {
             //console.log(breed_name);
             breedsHTML = breedsHTML +
-                `
-            <div>
-            <input type="checkbox" id="${breed_name}" name="${breed_name}" value="${breed_name}">
-            <label for="${breed_name}">${breed_name}</label>
+                `<div>
+            <input type="checkbox" name="breeds" id="${breed.id}" value="${breed.name}" onchange="breedChanged(this)" />
+            <label for="${breed.id}">${breed.name}</label>
             </div>`;
         }
         breedsHTML = breedsHTML + `</div></div>`;
     }
-
-
-    // for (let breed_name of dog_breeds_to_print_arr) {
-    //     console.log(breed_name);
-    //     breedsHTML = breedsHTML +
-    //     `<div>
-    //     <input type="checkbox" id="${breed_name}" name="${breed_name}" value="${breed_name}">
-    //     <label for="${breed_name}">${breed_name}</label>
-    //     </div>`;
-    // }
-    console.log(breedsHTML);
+    //console.log(breedsHTML);
     document.getElementById("breeds-carousel").innerHTML = breedsHTML;
     document.getElementById("breed-list-0").classList.add('active');
 }
@@ -149,7 +180,7 @@ function printDog(data) {
 function printDogAll() {
     console.log(dog_arr);
     for (let i = 0; i < dog_arr.length; i++) {
-        console.log(dog_arr[i][0].id);
+        //console.log(dog_arr[i][0].id);
     }
 }
 
@@ -168,7 +199,7 @@ function shuffleDogCards() {
     }
 }
 
-function printDogCards() {
+function printDogCardsToHTML() {
     let dog_cards = "";
     let card_id = 1;
     for (let dog of dog_arr) {
@@ -176,12 +207,12 @@ function printDogCards() {
         //console.log("Url: " + dog[0].url);
         dog_cards = dog_cards +
             `<div class="flip-card m-2" style="width:150px;height:150px;">
-            <div class="flip-card-inner ${dog[0].id}" id="${card_id}" onclick="card_clicked(this)">
+            <div class="flip-card-inner ${dog.id}" id="card-${card_id}" onclick="card_clicked(this)">
                 <div class="front">
                     <img src="doggo.jpg" style="width:150px;height:150px;">
                 </div>
                 <div class="back">
-                    <img src="${dog[0].url}" style="width:150px;height:150px;">
+                    <img src="${dog.url}" style="width:150px;height:150px;">
                 </div>
             </div>
         </div>`
@@ -295,7 +326,6 @@ function startGame() {
     correct_selected_pairs = 0;
     tried_pairs = 0;
     document.getElementById("tried_pairs").innerHTML = tried_pairs;
-    //TODO get current selected breeds
 
     //Clear dogArray
     dog_arr = [];
@@ -315,7 +345,50 @@ function startGame() {
             alert("Eror while specifying pairs. Default: 4 pairs");
             playset_pairs = 4;
     }
-    getDogs(playset_pairs);
+    buildAPICall();
+}
+
+
+function buildAPICall() {
+    let breeds = cur_breed_selection_id_arr;
+    let playsets = playset_pairs;
+    if (breeds.length == 0) {
+        getDogs(playsets);
+        return;
+    }
+    console.log("breeds: " + breeds);
+    let API_call_arr = new Array(); // Array holding the limit and breed id at each index to minimize the amount of API requests
+
+    //fill API call array with the breeds ids and set the limit for all breeds to zero
+    for (let breed_id of breeds) {
+        console.log("BreedID: " + breed_id);
+        API_call_arr.push({ limit: 0, id: breed_id }) //fuck
+    }
+    console.log("API_call_arr: " + API_call_arr);
+
+    let neededCards = playsets;
+    var limitplus = 1;
+
+    while (limitplus == 1) {
+        for (let index in API_call_arr) {
+            if (neededCards == 0) {
+                limitplus = 0;
+            }
+            API_call_arr[index].limit = API_call_arr[index].limit + limitplus
+            neededCards--
+        }
+    }
+    console.log(API_call_arr);
+
+    for (let i = 0; i < API_call_arr.length; i++) {
+        if (API_call_arr[i].limit > 0) {
+            // console.log("Request limit: "+ API_call_arr[i].limit);
+            // console.log("Request breed_id: "+ API_call_arr[i].id);
+            getDogs(API_call_arr[i].limit, API_call_arr[i].id);
+        } else {
+            //Sorry no Api calls for breed_id with limit = 0
+        }
+    }
 }
 
 function resetGame() {
@@ -327,6 +400,12 @@ function resetGame() {
     //reset current cards
     dog_arr = [];
     //reset current breed selection
+    cur_breed_selection_id_arr = [];
+    cur_breed_selection_name_arr = [];
+    document.getElementById("selected_breeds").innerHTML = "Selected breeds:";
+    //uncheck current breed selection with reprinting the breed selection
+    printBreeds();
+
     alert("The game is reset!");
 }
 
